25546f9f0105216020ad15b5e839b353cd62a632
Daniel Mori <danielstelzer@gmail.com>
2024-09-06 11:43:26 -0300

    Initial Commit

---
+++ Tests/DMCachedImageTests/DMCachedImageTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import DMCachedImage
+
+final class DMCachedImageTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
