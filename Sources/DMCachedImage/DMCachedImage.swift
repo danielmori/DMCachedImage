de8624e6ab4452ed620019e1cebe8db4da4a5610
Daniel Mori <danielstelzer@gmail.com>
2024-09-11 10:43:11 -0300

    Change in access control for DMCachedImage

--- Sources/DMCachedImage/DMCachedImage.swift
+++ Sources/DMCachedImage/DMCachedImage.swift
@@ -4,20 +4,20 @@
 import SwiftUI
 import SwiftData
 
-struct DMCachedImage<Content>: View where Content: View {
+public struct DMCachedImage<Content>: View where Content: View {
     private let url: URL
     private let scale: CGFloat
     private let transaction: Transaction
     private let content: (AsyncImagePhase) -> Content
 
-    init(url: URL, scale: CGFloat = 1.0, transaction: Transaction = Transaction(), @ViewBuilder content: @escaping (AsyncImagePhase) -> Content) {
+    public init(url: URL, scale: CGFloat = 1.0, transaction: Transaction = Transaction(), @ViewBuilder content: @escaping (AsyncImagePhase) -> Content) {
         self.url = url
         self.scale = scale
         self.transaction = transaction
         self.content = content
     }
     
-    var body: some View {
+    public var body: some View {
         if let cached = DMImageDatabase.shared.loadImage(fromURL: url.absoluteString) {
             content(.success(cached))
         } else {
